5da509455d4069d961b3a50370bb4d0dbb5f402f
Nicholas Yoannou <nicholas@nicholas.dev>
2026-09-13 19:49:05 +0100

    Simplify Windows launcher

--- mount/mount-windows.cmd
+++ mount/mount-windows.cmd
@@ -1,8 +1,6 @@
 @echo off
 setlocal DisableDelayedExpansion
-set "sshSwitchPowerShell=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
-if exist "%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\powershell.exe" set "sshSwitchPowerShell=%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\powershell.exe"
-"%sshSwitchPowerShell%" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0mount-windows.ps1"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0mount-windows.ps1"
 set "sshSwitchExit=%errorlevel%"
 if "%sshSwitchExit%"=="0" exit /b 0
 echo.
